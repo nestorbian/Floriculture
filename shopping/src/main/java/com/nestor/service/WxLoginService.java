e78e5e1be07b8c34e4026d9875b3d5a509f13179
brighthz <1296584078@qq.com>
2019-02-23 19:28:30 +0800

    fix small bug

--- shopping/src/main/java/com/nestor/service/WxLoginService.java
+++ shopping/src/main/java/com/nestor/service/WxLoginService.java
@@ -1,5 +1,7 @@
 package com.nestor.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,7 +14,8 @@ public interface WxLoginService extends JpaRepository<WxUser, String> {
 	//public WxUser saveAndFlush(WxUser wxUser);
 	
 	//原生SQL实现更新方法接口
-	@Query(value = "update WxUser set openid=?1,session_key=?2,thirdSession=?3,unionid=?4 where openid=?1 ", nativeQuery = true)  
+	@Transactional
+	@Query(value = "update t_wx_user set session_key=?2,third_session=?3,unionid=?4 where openid=?1 ", nativeQuery = true)  
 	@Modifying  
 	public void updateOne(String openid,String session_key,String setThirdSession,String unionid); 
 
