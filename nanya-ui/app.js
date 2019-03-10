045a9847599f4398e51154202986f2427c25dff9
brighthz <1296584078@qq.com>
2019-03-10 19:41:19 +0800

    add user info page

--- nanya-ui/app.js
+++ nanya-ui/app.js
@@ -11,29 +11,25 @@ App({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              console.log(this.globalData.userInfo)
-              console.log(res);
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+    }),
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: res => {
+                // 可以将 res 发送给后台解码出 unionId
+                this.globalData.userInfo = res.userInfo;
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                if (this.userInfoReadyCallback) {
+                  this.userInfoReadyCallback(res)
+                }
               }
-            }
-          })
+            })
+          }
         }
-      }
-    })
+      })
   },
   globalData: {
     userInfo: null
@@ -48,5 +44,8 @@ App({
         console.log(res.data)
       }
     })
+  },
+  // 获取用户信息
+  tologin : function(){
   }
 })
---
+++ nanya-ui/pages/mine/mine.js
@@ -0,0 +1,106 @@
+// pages/mine/mine.js
+import Dialog from '../../miniprogram_npm/vant-weapp/dialog/dialog';
+var app = getApp()
+Page({
+  data: {
+    userInfo: {},
+    motto: 'Hello World',
+    show:false,
+    // orderItems
+    orderItems: [
+      {
+        typeId: 0,
+        name: '待付款',
+        url: 'bill',
+        imageurl: 'pending-payment'
+      },
+      {
+        typeId: 1,
+        name: '待发货',
+        url: 'bill',
+        imageurl: 'exchange'
+      },
+      {
+        typeId: 2,
+        name: '待收货',
+        url: 'bill',
+        imageurl: 'tosend'
+      },
+      {
+        typeId: 3,
+        name: '待评价',
+        url: 'bill',
+        imageurl: 'records'
+      },
+      {
+        typeId: 4,
+        name: '售后服务',
+        url: 'bill',
+        imageurl: 'chat'
+      }
+    ],
+  },
+  //事件处理函数
+  toOrder: function () {
+    wx.navigateTo({
+      url: '../order/order'
+    })
+  },
+  login :function(){
+      wx.getUserInfo({
+        success: res => {
+          // 可以将 res 发送给后台解码出 unionId
+          app.globalData.userInfo = res.userInfo
+          this.data.userInfo = app.globalData.userInfo;
+          this.setData({
+            "userinfo": app.globalData.userInfo
+          })
+          // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+          // 所以此处加入 callback 以防止这种情况
+          if (app.userInfoReadyCallback) {
+            app.userInfoReadyCallback(res)
+          }
+        }
+      })    
+  },
+  //初始化时获取用户信息
+  onLoad : function () {
+    var that = this;
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+        //调用应用实例的方法获取全局数据
+        that.login();
+        }}
+      })
+  },
+  //授权窗口信息
+  toLogin:function(){
+    this.setData({
+      show: true
+    });    
+  },
+  //关闭授权窗口
+  onClose: function (event) {
+    if (event.detail === 'confirm') {
+      // 异步关闭弹窗
+      setTimeout(() => {
+        this.setData({
+          show: false
+        });
+      }, 200);
+    } else {
+      this.setData({
+        show: false
+      });
+    }
+  },
+  //确认授权之后，官方提供用户信息
+  onConfirmed(e){
+    app.globalData.userInfo = e.detail.userInfo;
+    userInfo = e.detail.userInfo;
+  }
+  
+})
+ 
