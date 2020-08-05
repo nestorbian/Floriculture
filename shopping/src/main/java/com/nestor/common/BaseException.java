2e2cca911f72a6aeb17fff47de88fa633178ab59
nestor.bian <1756073941@q.com>
2020-08-05 17:07:33 +0800

    修改异常类

--- shopping/src/main/java/com/nestor/common/BaseException.java
+++ shopping/src/main/java/com/nestor/common/BaseException.java
@@ -16,9 +16,12 @@ public class BaseException extends RuntimeException {
     private static final long serialVersionUID = -3961087072958094598L;
     @Getter
     private int code;
+    @Getter
+    private String msg;
 
     public BaseException(int code, String msg) {
-        super(msg);
+        super(String.format("异常信息: code:[%d], msg:[%s]", code, msg));
+        this.msg = msg;
         this.code = code;
     }
 }
